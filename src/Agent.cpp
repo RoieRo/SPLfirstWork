8902e4a59e06f9a7bb6246edf7bd4518b25cb92a
Aviv Day <avivday@gmail.com>
2022-11-08 23:03:06 +0200

    Roie look, it's me

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -3,6 +3,7 @@
 Agent::Agent(int agentId, int partyId, SelectionPolicy *selectionPolicy) : mAgentId(agentId), mPartyId(partyId), mSelectionPolicy(selectionPolicy)
 {
     // You2 can change the implementation of the constructor, but not the signature!
+    // Roie, what is a constructor?
 }
 
 int Agent::getId() const
